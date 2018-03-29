74c1c1aae63621203d83b2d2a644e6c2ea7e0376
Piotr Gliźniewicz <p.glizniewicz@netpr.pl>
2018-03-29 08:33:57 +0200

    Prototyping

--- src/login-page/LoginPage.jsx
+++ src/login-page/LoginPage.jsx
@@ -1,52 +1,108 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux';
+import React, {Component} from 'react'
+import {connect} from 'react-redux';
 
 @connect(s => s)
 export default class LoginPage extends Component {
 
     state = {
-        euro: []
-    }
+        euro: [],
+        foundCards: {},
+        foundCardNames: {},
+        chosen: [],
+        cardName: ""
+    };
 
     render() {
-        const { euro } = this.state;
         const euroRate = this.findEuro();
         return (
             <div>
+                <input value={this.state.cardName} onChange={this.findCardNames}></input>
+                {/* Podpowiedzi wyszukiwania TODO: debounce */
+                    this.state.foundCardNames.data &&
+                    <h1>{this.state.foundCardNames.total_cards}</h1>
+                }
                 {
+                    this.state.foundCardNames.data &&
+                    <ul>
+                        {
+                            this.state.foundCardNames.data.map(card => {
+                                return <li>{card.name}</li>// onClick ustaw this.state.cardName i odpal szukajkę
+                            })
+                        }
+                    </ul>
+                }
+
+                <button onClick={this.findCards}>Find cards</button>
+
+                {/* Wyniki wyszukiwania */
+                    this.state.foundCards.data &&
+                    <ul>
+                        {
+                            this.state.foundCards.data.map(print => {
+                                return <li>{print.set} {(print.eur * euroRate.mid).toFixed(2)} PLN</li>
+                            })
+                        }
+                    </ul>
+                }
+
+                {/* Wybrane karty */
+                    this.state.chosen &&
+                    <ul>
+                        {
+                            this.state.chosen.map(print => {
+                                return <li>{print.set} {(print.eur * euroRate.mid).toFixed(2)} PLN</li>
+                            })
+                        }
+                    </ul>
+                }
+
+                {/* Kurs EUR/PLN */
                     euroRate &&
-                    <div style={{padding: 30}}>
-                        <h1>Kurs euro</h1>
+                    <div>
                         <p>
-                            {euroRate.mid}
+                            Średni kurs EUR/PLN NBP {euroRate.mid}
                         </p>
                     </div>
                 }
-
             </div>
         )
     }
 
-    addTodo = () => {
-        const { todos } = this.props;
+    findCardNames = async (event) => {
+        this.setState({
+            cardName: event.target.value
+        });
+        const json = await fetch('https://api.scryfall.com/cards/search?&q=' + event.target.value)
+            .then(response => response.json());
 
-        const todo = {
-            name: `todo-${todos.length + 1}`
-        }
-        this.props.dispatch({
-            type: 'add-todo',
-            payload: todo
+        this.setState({
+            foundCardsNames: json
         })
+    };
+
+    setCardName = (event) => {
+        this.setState({
+            cardName: event.target.value
+        });
     }
 
+    findCards = async (event) => {
+        const json = await fetch('https://api.scryfall.com/cards/search?&q=!' + this.state.cardName + '&unique=prints&order=set&dir=desc')
+            .then(response => response.json());
+
+        this.setState({
+            foundCards: json
+        })
+    };
+
     findEuro = () => {
-        const { euro } = this.state;
+        const {euro} = this.state;
         if (euro.length === 0)
             return undefined;
 
         return euro[0].rates.find(rate => rate.code === 'EUR');
 
-    }
+    };
 
     async componentDidMount() {
         const json = await fetch('http://api.nbp.pl/api/exchangerates/tables/A/?format=JSON')
