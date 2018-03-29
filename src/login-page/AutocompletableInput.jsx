b7ec3ff4c32fe837eda0c4d378a1739e44ae9613
Kamil Perczyński <k.perczynski@netpr.pl>
2018-03-29 09:45:12 +0200

    autocomplete dla piotrusia <3

---
+++ src/login-page/AutocompletableInput.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+
+class AutocompletableInput extends Component {
+
+    state = {
+        sequenceNumber: 0,
+        dataSource: [],
+        duringInput: false,
+    }
+
+    render() {
+        const { value, onInput } = this.props
+        const { duringInput } = this.state;
+        return (
+            <div>
+                <input value={value} onChange={e => this.handleInput(e.target.value)} />
+                <span>{duringInput && 'input'}</span>
+            </div>
+        )
+    }
+
+    handleInput = (value) => {
+        const sequence = this.state.sequenceNumber + 1;
+        this.setState({ duringInput: true, sequenceNumber: sequence })
+        this.props.onInput(value);
+        setTimeout(() => {
+            if (this.state.sequenceNumber === sequence) {
+                this.setState({ duringInput: false })
+                this.props.onDataRequest()
+            }
+        }, 250)
+    }
+}
+
+export default AutocompletableInput
--- src/login-page/LoginPage.jsx
+++ src/login-page/LoginPage.jsx
@@ -1,91 +1,118 @@
-import React, {Component} from 'react'
-import {connect} from 'react-redux';
+// @flow
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import AutocompletableInput from './AutocompletableInput';
 
-export default class LoginPage extends Component {
+interface CardName {
+    name: string
+}
+
+interface CardHints {
+    data: Array<CardName>,
+    total_cards: number
+}
+
+type State = {
+    cardName: string,
+    euro: any,
+    foundCardNames: CardHints,
+    foundCards: any
+}
+
+interface Cards {
+    data: Array<Card>
+}
+
+interface Card {
+    mid: number,
+    eur: string
+}
+
+export default class LoginPage extends Component<void, State> {
 
     state = {
         euro: [],
-        foundCards: {},
-        foundCardNames: {},
+        foundCards: {
+            data: [],
+        },
+        foundCardNames: {
+            data: [],
+            total_cards: 0
+        },
         chosen: [],
         cardName: ""
-    };
+    }
 
     render() {
         const euroRate = this.findEuro();
+        const { foundCardNames } = this.state;
         return (
             <div>
-                <input value={this.state.cardName} onChange={this.findCardNames}></input>
-                {/* Podpowiedzi wyszukiwania TODO: debounce */
-                    this.state.foundCardNames.data &&
-                    <h1>{this.state.foundCardNames.total_cards}</h1>
-                }
-                {
-                    this.state.foundCardNames.data &&
-                    <ul>
-                        {
-                            this.state.foundCardNames.data.map(card => {
-                                return <li>{card.name}</li>// onClick ustaw this.state.cardName i odpal szukajkę
-                            })
-                        }
-                    </ul>
-                }
+                <div style={{ flexDirection: 'row' }}>
+                    <AutocompletableInput
+                        onDataRequest={this.fetchHints}
+                        value={this.state.cardName}
+                        onInput={value => this.setState({ cardName: value })} />
+                    <button onClick={this.findCards}>Szukaj</button>
+                </div>
+                <div>
+                    {
+                        foundCardNames.data &&
+                        <ul>
+                            {
+                                this.state.foundCardNames.data.map((card, i) => {
+                                    return <li key={i}>{card.name || ''}</li>// onClick ustaw this.state.cardName i odpal szukajkę
+                                })
+                            }
+                        </ul>
+                    }
+                </div>
 
-                <button onClick={this.findCards}>Find cards</button>
+                <div>
+                    {/* Podpowiedzi wyszukiwania TODO: debounce */
+                        this.state.foundCardNames.data &&
+                        <h1>{foundCardNames.total_cards}</h1>
+                    }
 
-                {/* Wyniki wyszukiwania */
-                    this.state.foundCards.data &&
-                    <ul>
-                        {
-                            this.state.foundCards.data.map(print => {
-                                return <li>{print.set} {(print.eur * euroRate.mid).toFixed(2)} PLN</li>
-                            })
-                        }
-                    </ul>
-                }
+                    {/* Wyniki wyszukiwania */
+                        this.state.foundCards.data &&
+                        <ul>
+                            {
+                                this.state.foundCards.data.map((print, i) => {
+                                    return <li key={i}>{print.set} {(print.eur * euroRate.mid).toFixed(2)} PLN</li>
+                                })
+                            }
+                        </ul>
+                    }
+                </div>
 
-                {/* Wybrane karty */
-                    this.state.chosen &&
-                    <ul>
-                        {
-                            this.state.chosen.map(print => {
-                                return <li>{print.set} {(print.eur * euroRate.mid).toFixed(2)} PLN</li>
-                            })
-                        }
-                    </ul>
-                }
+                <div>
+                    {/* Wybrane karty */
+                        this.state.chosen &&
+                        <ul>
+                            {
+                                this.state.chosen.map(print => {
+                                    return <li>{print.set} {(print.eur * euroRate.mid).toFixed(2)} PLN</li>
+                                })
+                            }
+                        </ul>
+                    }
 
-                {/* Kurs EUR/PLN */
-                    euroRate &&
-                    <div>
-                        <p>
-                            Średni kurs EUR/PLN NBP {euroRate.mid}
-                        </p>
-                    </div>
-                }
+                    {/* Kurs EUR/PLN */
+                        euroRate &&
+                        <div>
+                            <p>
+                                Średni kurs EUR/PLN NBP {euroRate.mid}
+                            </p>
+                        </div>
+                    }
+                </div>
+
             </div>
         )
     }
 
-    findCardNames = async (event) => {
-        this.setState({
-            cardName: event.target.value
-        });
-        const json = await fetch('https://api.scryfall.com/cards/search?&q=' + event.target.value)
-            .then(response => response.json());
-
-        this.setState({
-            foundCardsNames: json
-        })
-    };
-
-    setCardName = (event) => {
-        this.setState({
-            cardName: event.target.value
-        });
-    }
-
-    findCards = async (event) => {
+    findCards = async (event: any) => {
         const json = await fetch('https://api.scryfall.com/cards/search?&q=!' + this.state.cardName + '&unique=prints&order=set&dir=desc')
             .then(response => response.json());
 
@@ -95,13 +122,27 @@ export default class LoginPage extends Component {
     };
 
     findEuro = () => {
-        const {euro} = this.state;
+        const { euro } = this.state;
         if (euro.length === 0)
             return undefined;
 
         return euro[0].rates.find(rate => rate.code === 'EUR');
 
     };
+
+    fetchHints = async () => {
+        const { cardName } = this.state;
+        try {
+            const json = await fetch(`https://api.scryfall.com/cards/search?q=${cardName}`)
+                .then(response => response.json());
+            this.setState({
+                foundCardNames: json
+            });
+        }
+        catch (err) {
+            console.log('no cards found')
+        }
+    }
 
     async componentDidMount() {
         const json = await fetch('https://api.nbp.pl/api/exchangerates/tables/A/?format=JSON')
